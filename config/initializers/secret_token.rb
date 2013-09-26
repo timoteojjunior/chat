8ffc16f2722161584bb0123ff1e74f374bb9093e
Timoteo Junior <timoteojjunior@gmail.com>
2013-09-26 09:34:14 +0200

    Created the chat app, and its functionalities

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chat::Application.config.secret_token = 'acb004478cc89fcb0f9c5da08853b8ef7a1ba03aabde4ca8e41a15086a5fce2d4dde56dd927cd72f82b669cfc6afa2b809c60d36f6673879659e7f9f0a15d51e'
